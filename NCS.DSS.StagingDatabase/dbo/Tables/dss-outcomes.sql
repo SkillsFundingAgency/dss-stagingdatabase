295779ff7ff73975bcc7f1a03fdfc83260be611a
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2019-02-07 15:33:28 +0000

    Add SP for ChangeFeed Outcomes

--- NCS.DSS.StagingDatabase/dbo/Tables/dss-outcomes.sql
+++ NCS.DSS.StagingDatabase/dbo/Tables/dss-outcomes.sql
@@ -2,11 +2,13 @@
     [id]                       UNIQUEIDENTIFIER NULL,
     [CustomerId]               UNIQUEIDENTIFIER NULL,
     [ActionPlanId]             UNIQUEIDENTIFIER NULL,
-    [OutcomeType]              INT              NULL,
+	[SubcontractorId]		   VARCHAR(50)		 NULL,
+    [OutcomeType]              INT               NULL,
     [OutcomeClaimedDate]       datetime2         NULL,
     [OutcomeEffectiveDate]     datetime2         NULL,
+	[ClaimedPriorityGroup]     INT               NULL,
     [TouchpointId]             VARCHAR (max)     NULL,
     [LastModifiedDate]         datetime2         NULL,
-    [LastModifiedTouchpointId] VARCHAR (max)     NULL
+    [LastModifiedTouchpointId] VARCHAR (max)     NULL 
 );
 
