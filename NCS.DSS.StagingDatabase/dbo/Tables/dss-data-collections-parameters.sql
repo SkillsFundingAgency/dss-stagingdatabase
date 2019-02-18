7c2b520010ba1352829b7b2e2d9b7b938304196b
Dan Hardwicke <dan.hardwicke@outlook.com>
2019-02-18 10:19:20 +0000

    Added Data Collections parameters table to solution

--- NCS.DSS.StagingDatabase/dbo/Tables/dss-data-collections-parameters.sql
+++ NCS.DSS.StagingDatabase/dbo/Tables/dss-data-collections-parameters.sql
@@ -1,10 +1,4 @@
-﻿IF OBJECT_ID('[dbo].[dss-data-collections-parameters]') IS NOT NULL
-BEGIN
-	DROP TABLE [dbo].[dss-data-collections-parameters]
-END
-GO
-
-CREATE TABLE [dbo].[dss-data-collections-parameters](
+﻿CREATE TABLE [dbo].[dss-data-collections-parameters](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[ParameterName] [nvarchar](50) NOT NULL,
 	[ParameterValue] [nvarchar](50) NOT NULL,
@@ -13,4 +7,3 @@ PRIMARY KEY CLUSTERED
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
 )
-
