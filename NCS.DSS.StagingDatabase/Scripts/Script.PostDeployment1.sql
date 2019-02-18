d1a38320fb45af0a4259942aad16fd84b9f587e5
Dan Hardwicke <dan.hardwicke@outlook.com>
2019-02-18 10:45:42 +0000

    Tidy up Parameters table create and post deployment script

--- NCS.DSS.StagingDatabase/Scripts/Script.PostDeployment1.sql
+++ NCS.DSS.StagingDatabase/Scripts/Script.PostDeployment1.sql
@@ -10,6 +10,6 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
-INSERT INTO [dss-data-collections-parameters] (ParameterName, ParameterValue) VALUES ('MinAge', '18')
-INSERT INTO [dss-data-collections-parameters] (ParameterName, ParameterValue) VALUES ('MaxAge', '100')
-INSERT INTO [dss-data-collections-parameters] (ParameterName, ParameterValue) VALUES ('ContractStartDate', '2018/10/01')
+INSERT INTO [dss-data-collections-params] (ParameterName, ParameterValue) VALUES ('MinAge', '18')
+INSERT INTO [dss-data-collections-params] (ParameterName, ParameterValue) VALUES ('MaxAge', '100')
+INSERT INTO [dss-data-collections-params] (ParameterName, ParameterValue) VALUES ('ContractStartDate', '2018/10/01')
