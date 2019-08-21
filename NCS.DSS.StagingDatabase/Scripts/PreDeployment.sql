669ffd29d2654baa1c19f7b0ed92602c0edec204
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2019-08-21 11:22:43 +0100

    update diversity table lastmodifiedby

---
+++ NCS.DSS.StagingDatabase/Scripts/PreDeployment.sql
@@ -0,0 +1,21 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+IF COL_LENGTH(N'dss-diversitydetails',  N'LastModifiedTouchpointId') IS NOT NULL
+BEGIN
+	EXEC sp_rename 'dss-diversitydetails.LastModifiedTouchpointId', 'LastModifiedBy', 'COLUMN';  
+END;
+
+IF COL_LENGTH(N'dss-diversitydetails-history',  N'LastModifiedTouchpointId') IS NOT NULL
+BEGIN
+	EXEC sp_rename 'dss-diversitydetails-history.LastModifiedTouchpointId', 'LastModifiedBy', 'COLUMN';  
+END;
