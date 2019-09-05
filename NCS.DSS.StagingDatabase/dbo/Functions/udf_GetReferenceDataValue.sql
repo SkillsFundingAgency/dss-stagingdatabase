6f429910abd529f88503a7eb8a63c2a88808db99
k.brandon <kevin.brandonuk@outlook.com>
2019-09-05 09:28:26 +0100

    Added function to get reference data value.

---
+++ NCS.DSS.StagingDatabase/dbo/Functions/udf_GetReferenceDataValue.sql
@@ -0,0 +1,30 @@
+-------------------------------------------------------------------------------
+-- Authors:      Kevin Brandon
+-- Created:      05/09/2019
+-- Purpose:      Provide a single point to call to get reference data value.
+--  
+-------------------------------------------------------------------------------
+-- Modification History
+-- Initial creation.
+-- 
+--            
+-- Copyright © 2019, ESFA, All Rights Reserved
+-------------------------------------------------------------------------------
+CREATE FUNCTION [dbo].[udf_GetReferenceDataValue]( @resource VARCHAR(50), @name VARCHAR(50), @criteria INT, @default VARCHAR(50) )
+RETURNS VARCHAR(50)
+AS
+BEGIN
+	DECLARE @ret varchar(50);
+
+	SELECT @ret = description 
+				FROM [DBO].[dss-reference-data]  
+				WHERE 
+				[DBO].[dss-reference-data].[Resource] = @resource AND 
+				[DBO].[dss-reference-data].[name] = @name AND 
+				[DBO].[dss-reference-data].[value] = @criteria
+
+	IF (@ret IS NULL) 
+		SET @ret = @default;
+
+	RETURN @ret;  
+END;
