1e5ea0c75e6bb0207ca49f325a202a920d6b09f0
PradeepVoorukonda <87078845+PradeepVoorukonda@users.noreply.github.com>
2023-03-21 16:10:13 +0000

    fixed script

--- NCS.DSS.StagingDatabase/PowerBI/Tables/PowerBI.dss-pbi-financialyear.sql
+++ NCS.DSS.StagingDatabase/PowerBI/Tables/PowerBI.dss-pbi-financialyear.sql
@@ -2,13 +2,7 @@ CREATE TABLE [PowerBI].[dss-pbi-financialyear](
 	[FinancialYear] [varchar](9) NOT NULL,
 	[StartDateTime] [datetime2](7) NOT NULL,
 	[EndDateTime] [datetime2](7) NOT NULL,
-	[Comments] [varchar](max) NULL
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-SET ANSI_PADDING ON
-GO
-ALTER TABLE [PowerBI].[dss-pbi-financialyear] ADD  CONSTRAINT [pk-dss-pbi-financialyear] PRIMARY KEY CLUSTERED 
-(
-	[FinancialYear] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+	[Comments] [varchar](max) NULL,
+    CONSTRAINT [pk-dss-pbi-financialyear] PRIMARY KEY CLUSTERED ([FinancialYear] ASC)
+) 
 GO
