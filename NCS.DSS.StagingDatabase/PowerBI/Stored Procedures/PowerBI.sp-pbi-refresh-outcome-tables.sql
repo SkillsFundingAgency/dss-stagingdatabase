5e1e40e11db4ec401153002f6502c33e04734c9a
Pradeep Kumar Voorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2024-04-18 14:48:19 +0100

    refresh stored procedure update

---
+++ NCS.DSS.StagingDatabase/PowerBI/Stored Procedures/PowerBI.sp-pbi-refresh-outcome-tables.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE [PowerBI].[sp-pbi-refresh-outcome-tables](@pr1 int)
+AS
+BEGIN
+    TRUNCATE TABLE [PowerBI].[dss-pbi-outcome]
+                ;
+                INSERT INTO [PowerBI].[dss-pbi-outcome]
+                SELECT * FROM [PowerBI].[v-dss-pbi-outcome]
+                ;
+                TRUNCATE TABLE [PowerBI].[dss-pbi-outcomeactualvolume]
+                ;
+                INSERT INTO [PowerBI].[dss-pbi-outcomeactualvolume]
+                SELECT * FROM [PowerBI].[v-dss-pbi-outcomeactualvolume] WHERE TouchpointID IS NOT NULL
+END
