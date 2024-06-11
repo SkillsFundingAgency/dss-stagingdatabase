4e408ff0a684c19027a0273495a5d555aafddd2e
Pradeep Kumar Voorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2024-06-11 08:18:00 +0100

    improvements to outcome actuals

--- NCS.DSS.StagingDatabase/PowerBI/Tables/PowerBI.dss-pbi-financialyear.sql
+++ NCS.DSS.StagingDatabase/PowerBI/Tables/PowerBI.dss-pbi-financialyear.sql
@@ -3,6 +3,7 @@ CREATE TABLE [PowerBI].[dss-pbi-financialyear](
 	[StartDateTime] [datetime2](7) NOT NULL,
 	[EndDateTime] [datetime2](7) NOT NULL,
 	[Comments] [varchar](max) NULL,
+	[CurrentYear] BIT NULL,
     CONSTRAINT [pk-dss-pbi-financialyear] PRIMARY KEY CLUSTERED ([FinancialYear] ASC)
 ) 
 GO
