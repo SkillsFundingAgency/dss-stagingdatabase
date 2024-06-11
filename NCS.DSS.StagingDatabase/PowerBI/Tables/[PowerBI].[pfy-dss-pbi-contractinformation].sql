178e752993b2b7339bb537b976f097ddb4000685
madhu-pasumarthi <madhu.pasumarthi@netcompany.com>
2024-06-11 15:44:50 +0100

    v-dss-pbi-contractinformation

---
+++ NCS.DSS.StagingDatabase/PowerBI/Tables/[PowerBI].[pfy-dss-pbi-contractinformation].sql
@@ -0,0 +1,11 @@
+CREATE TABLE [PowerBI].[pfy-dss-pbi-contractinformation](
+[TouchpointID]int,
+[ProfileCategory] varchar(23),
+[Date] datetime,
+[Fiscal Year] varchar(9),
+[ProfileCategoryValue] decimal(38,2)
+	
+) ON [PRIMARY]
+GO
+
+
