44038f208ba770dffcc1ae538975f16b55d80aca
Pradeep Kumar Voorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2024-07-01 10:22:49 +0100

    fix deploymnet issue again!

--- NCS.DSS.StagingDatabase/Scripts/Create_PFYTables.sql
+++ NCS.DSS.StagingDatabase/Scripts/Create_PFYTables.sql
@@ -167,30 +167,3 @@ BEGIN
 END
 
 
-IF EXISTS (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'pfy-dss-pbi-outcomeprofilevolume' AND TABLE_SCHEMA = 'PowerBI')
-BEGIN	
-	DROP TABLE  [PowerBI].[pfy-dss-pbi-outcomeprofilevolume];
-END
-ELSE
-BEGIN
-	CREATE TABLE [PowerBI].[pfy-dss-pbi-outcomeprofilevolume](
-		[TouchpointID] varchar(4) not null
-        ,[ProfileCategory] varchar(10) not null
-        ,[PriorityOrNot] varchar(2) not null
-        ,[PeriodMonth] int not null
-		,[date] datetime not null
-        ,[PeriodYear] varchar(9) not null
-        ,[OutcomeNumber] decimal(10,2)
-        ,[YTD_OutcomeNumber] decimal(10,2)
-	CONSTRAINT [pk-pfy-dss-pbi-outcomeprofilevolume] PRIMARY KEY CLUSTERED 
-	(
-		TouchpointID ASC,
-		[ProfileCategory] ASC,
-		[PriorityOrNot] ASC,
-		[PeriodMonth] ASC,
-		[date] ASC,
-		[PeriodYear] ASC
-		)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-	)
-END
-
