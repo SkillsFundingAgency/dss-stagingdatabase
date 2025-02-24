35d2b1e11e578c5f8771c8d712f1445f9f641d42
Pradeep Voorukonda <87078845+pvoorukonda-DFE@users.noreply.github.com>
2025-02-24 13:53:56 +0000

    fixing build fail

--- NCS.DSS.StagingDatabase/PowerBI/Tables/PowerBI.dss-pbi-actualpaymentsmade.sql
+++ NCS.DSS.StagingDatabase/PowerBI/Tables/PowerBI.dss-pbi-actualpaymentsmade.sql
@@ -4,7 +4,7 @@ CREATE TABLE [PowerBI].[dss-pbi-actualpaymentsmade]
 	[FinancialYear] VARCHAR(9) NOT NULL,
     [MonthID] INT NOT NULL,
     [CategoryName] VARCHAR(32) NOT NULL,
-    [PaymentMade] INT NULL, 
+    [PaymentMade] INT NOT NULL, 
 	CONSTRAINT [pk-dss-pbi-actualpaymentsmade] PRIMARY KEY CLUSTERED ([TouchpointID] ,[FinancialYear],[MonthID],[CategoryName]) 
 )
 
