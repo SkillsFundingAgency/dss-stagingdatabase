066cc976a7b6304ba4d12438ad886079edc2cacc
mlarkowski-DFE <178817151+mlarkowski-DFE@users.noreply.github.com>
2025-08-15 16:24:19 +0100

    refactored [PowerBI].[v-dss-pbi-servicefeeperformance]

--- NCS.DSS.StagingDatabase/PowerBI/Views/PowerBI.v-dss-pbi-servicefeeperformance.sql
+++ NCS.DSS.StagingDatabase/PowerBI/Views/PowerBI.v-dss-pbi-servicefeeperformance.sql
@@ -1,44 +1,5 @@
 CREATE VIEW [PowerBI].[v-dss-pbi-servicefeeperformance] 
 AS 
-	WITH MYServiceFee 
-	(
-		[TouchpointID] 
-		,[ProfileCategory] 
-		,[Date] 
-		,[Fiscal Year] 
-		,[Financial Profile total] 
-		,[ProfileCategoryValue] 
-		,[Financial Profile YTD] 
-		,[ProfileCategoryValueYTD] 
-	)
-	AS 
-	(
-		SELECT 
-			AF.[TouchpointID] 
-			,'Service Fee' AS [ProfileCategory]
-			,PD.[Date] 
-			,PD.[Fiscal Year]
-			,AF.[Financial Profile total] 
-			,AF.[Financial Profile total] * ((100 - ISNULL(SE.[ReferenceValue], 0)) / 100) AS [ProfileCategoryValue] 
-			,AF.[Financial Profile YTD] 
-			,SUM(AF.[Financial Profile total] * ((100 - ISNULL(SE.[ReferenceValue], 0)) / 100)) 
-				OVER(PARTITION BY AF.[TouchpointID], PD.[Fiscal Year] ORDER BY PD.[Date]) AS [ProfileCategoryValueYTD] 
-		FROM [PowerBI].[v-dss-pbi-outcomeprofilefact] AS AF 
-		INNER JOIN [PowerBI].[v-dss-pbi-date] AS PD 
-		ON AF.[Date] = PD.[Date] 
-		LEFT OUTER JOIN [PowerBI].[dss-pbi-servicefeededuction] AS SE 
-		ON AF.[TouchpointID] = SE.[TouchpointID] 
-		AND AF.[Date] = SE.[CalendarDate] 
-		WHERE AF.[Outcome ID] = 11 
-		GROUP BY 
-			AF.[TouchpointID] 
-			,PD.[Date] 
-			,PD.[Fiscal Year] 
-			,AF.[Financial Profile total] 
-			,AF.[Financial Profile total] * ((100 - ISNULL(SE.[ReferenceValue], 0)) / 100) 
-			,AF.[Financial Profile YTD] 
-	)
-
 	SELECT 
 		SF.[TouchpointID]
 		,SF.[Date]
@@ -46,7 +7,7 @@ AS
 		,SF.[ProfileCategoryValue]
 		,SF.[ProfileCategoryValueYTD] 
 		,PP.[ProfileCategoryValue] AS [ProfileCategoryValueFullYear] 
-	FROM MYServiceFee AS SF 
+	FROM [PowerBI].[v-dss-pbi-servicefeeperformance-MYServiceFee]  AS SF 
 	INNER JOIN [PowerBI].[dss-pbi-primeprofile] AS PP 
 	ON SF.[TouchpointID] = PP.[TouchpointID] 
 	AND SF.[Date] BETWEEN PP.[StartDateTime] AND PP.[EndDateTime] 
@@ -60,7 +21,7 @@ AS
 		,(SF.[ProfileCategoryValue] / SF.[Financial Profile Total]) * 100 AS [ProfileCategoryValue] 
 		,(SF.[ProfileCategoryValueYTD] / SF.[Financial Profile YTD]) * 100 AS [ProfileCategoryValueYTD] 
 		,(SF.[ProfileCategoryValueYTD] / PP.[ProfileCategoryValue]) * 100 AS [ProfileCategoryValueFullYear] 
-	FROM MYServiceFee AS SF 
+	FROM [PowerBI].[v-dss-pbi-servicefeeperformance-MYServiceFee]  AS SF 
 	INNER JOIN [PowerBI].[dss-pbi-primeprofile] AS PP 
 	ON SF.[TouchpointID] = PP.[TouchpointID] 
 	AND SF.[Date] BETWEEN PP.[StartDateTime] AND PP.[EndDateTime] 
