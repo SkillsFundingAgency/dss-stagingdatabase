ada9e4c3c1c154a5c6215952b4bf5e62629be358
mlarkowski-DFE <178817151+mlarkowski-DFE@users.noreply.github.com>
2025-10-20 15:47:57 +0100

    removed no longer needed actions from pre and post deployment scripts

--- NCS.DSS.StagingDatabase/Scripts/PreDeployment.sql
+++ NCS.DSS.StagingDatabase/Scripts/PreDeployment.sql
@@ -10,92 +10,18 @@
 --------------------------------------------------------------------------------------
 */
 
-IF COL_LENGTH(N'dbo.dss-diversitydetails',  N'LastModifiedTouchpointId') IS NOT NULL
-BEGIN
-	EXEC sp_rename 'dbo.dss-diversitydetails.LastModifiedTouchpointId', 'LastModifiedBy', 'COLUMN';  
-END;
-
-IF COL_LENGTH(N'dbo.dss-diversitydetails-history',  N'LastModifiedTouchpointId') IS NOT NULL
-BEGIN
-	EXEC sp_rename 'dbo.dss-diversitydetails-history.LastModifiedTouchpointId', 'LastModifiedBy', 'COLUMN';  
-END;
-
-IF COL_LENGTH(N'dbo.dss-collections',  N'TouchpointId') IS NOT NULL
-BEGIN
-	EXEC sp_rename 'dbo.dss-collections.TouchpointId', 'TouchPointId', 'COLUMN';  
-END;
-
-IF EXISTS(
-       SELECT 1
-       FROM   sys.columns
-       WHERE  NAME = 'EconomicShockCode'
-              AND [object_id] = OBJECT_ID('dbo.dss-employmentprogressions-history')
-              AND TYPE_NAME(system_type_id) = 'int'
-   )
-BEGIN
-	ALTER TABLE [dbo].[dss-employmentprogressions-history] ALTER COLUMN EconomicShockCode VARCHAR (50);
-END;
---- ALTER PAYMENTS MADE COLUMN TYPE TO DECIMAL 
-IF EXISTS(
-       SELECT 1
-       FROM   sys.columns
-       WHERE  NAME = 'PaymentMade'
-              AND [object_id] = OBJECT_ID('PowerBI.dss-pbi-actualpaymentsmade')
-              AND TYPE_NAME(system_type_id) = 'int'
-   )
-BEGIN
-	ALTER TABLE [PowerBI].[dss-pbi-actualpaymentsmade] ALTER COLUMN PaymentMade DECIMAL(12,5);
-END;
-
---- ALTER PaymentMade MADE COLUMN TYPE TO DECIMAL (18,5)
-IF EXISTS(
-       SELECT 1
-  FROM   sys.columns
-  WHERE  NAME = 'PaymentMade'
-         AND [object_id] = OBJECT_ID('PowerBI.dss-pbi-actualpaymentsmade')
-         AND TYPE_NAME(system_type_id) = 'decimal'
-		 and Precision =18
-		 and Scale =5
-   )
-BEGIN
---is this wrong?
-	ALTER TABLE [PowerBI].[dss-pbi-submission-pattern] ALTER COLUMN Percentage DECIMAL(12,5);
-END;
-
---- ALTER Percentage MADE COLUMN TYPE TO DECIMAL (12,5)
-IF EXISTS(
-       SELECT 1
-  FROM   sys.columns
-  WHERE  NAME = 'Percentage'
-         AND [object_id] = OBJECT_ID('PowerBI.dss-pbi-submission-pattern')
-         AND TYPE_NAME(system_type_id) = 'decimal'
-		 and Precision in (10,18)
-		 and Scale in (2,5)
-   )
-BEGIN
-	ALTER TABLE [PowerBI].[dss-pbi-submission-pattern] ALTER COLUMN Percentage DECIMAL(12,5);
-END;
-
-
---- CREATE TABLES REQUIRED TO STORE PFY DATA -----
-:r ./Create_PFYTables.sql
-
---Increased decimal places to [TargetCategoryValue]
+--Aligns the clustered key with that of table design
+--can be removed after next powerbi release
 ALTER TABLE [PowerBI].[dss-pbi-nationaltarget]
 DROP CONSTRAINT [pk-dss-pbi-nationaltarget];
 
 ALTER TABLE [PowerBI].[dss-pbi-nationaltarget]
-ALTER COLUMN [TargetCategoryValue] DECIMAL(8, 5) NOT NULL;
-
-ALTER TABLE [PowerBI].[dss-pbi-nationaltarget]
 ADD CONSTRAINT [pk-dss-pbi-nationaltarget] PRIMARY KEY CLUSTERED 
 (
-    [FinancialYear] ASC,
-    [ContractYear] ASC,
-    [PeriodMonth] ASC,
-    [PriorityOrNot] ASC,
-    [TargetCategory] ASC,
-    [TargetCategoryValue] ASC
+	[FinancialYear] ASC,
+	[PeriodMonth] ASC,
+	[PriorityOrNot] ASC,
+	[TargetCategory] ASC
 )
 WITH (
     STATISTICS_NORECOMPUTE = OFF,
