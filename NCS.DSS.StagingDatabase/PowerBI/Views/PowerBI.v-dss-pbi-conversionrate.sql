dc5292cebecb4c7a3d4ba3cd1bc745672a0f1a73
mpasumarthi <madhu.pasumarthi@education.gov.uk>
2026-01-28 13:36:13 +0000

    Adding CMD to CMO%

--- NCS.DSS.StagingDatabase/PowerBI/Views/PowerBI.v-dss-pbi-conversionrate.sql
+++ NCS.DSS.StagingDatabase/PowerBI/Views/PowerBI.v-dss-pbi-conversionrate.sql
@@ -12,14 +12,18 @@ SELECT
 	,AF.[Outcome ID]
 	,AF.[Date]
 	,ROUND((AF.[Outcome number] / IIF(PF.[Outcome number] = 0, 1, PF.[Outcome number])) * 100, 2) AS [Performance] 
-	,ROUND((AF.[YTD Outcome number] / IIF(PF.[YTD Outcome number] = 0, 1, PF.[YTD Outcome number])) * 100, 2) AS [Performance YTD] 
+	,ROUND(((AF.[YTD Outcome number]+DC.[YTD Outcome number])/ IIF(PF.[YTD Outcome number] = 0, 1, PF.[YTD Outcome number])) * 100, 2) AS [Performance YTD] 
 FROM 
 [PowerBI].[v-dss-pbi-conversionrate-myprofile] as AF
 INNER JOIN 
 [PowerBI].[v-dss-pbi-conversionrate-myprofile] AS PF     
-
 ON AF.[TouchpointID] = PF.[TouchpointID] 
 AND AF.[Date] = PF.[Date] 
+Left JOIN 
+[PowerBI].[v-dss-pbi-conversionrate-myprofile] AS DC     
+ON AF.[TouchpointID] = DC.[TouchpointID] 
+AND AF.[Date] = DC.[Date] 
+and DC.[Outcome ID]=8
 WHERE PF.[Outcome ID] = 7 -- Customer Numbers
 ;
 GO
