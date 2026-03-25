0b3a37f6dd80553c2980978f2f55d877cd65bf49
mpasumarthi <madhu.pasumarthi@education.gov.uk>
2026-03-25 12:19:33 +0000

    Update2026-2027asCurrentFinancialYear

--- NCS.DSS.StagingDatabase/Scripts/PostDeployment.sql
+++ NCS.DSS.StagingDatabase/Scripts/PostDeployment.sql
@@ -12,3 +12,30 @@ Post-Deployment Script Template
 
 -- FULL REFRESHE OF PFY DATA FOLLOWING DEPLOYMENT --- 
 --EXECUTE [PowerBI].[sp-pbi-refresh-all-data] @pr1 = 1;
+Begin Tran Add20262027FinancialYear
+-- Update all records to set CurrentYear = 0
+UPDATE [PowerBI].[dss-pbi-financialyear]
+SET [CurrentYear] = 0;    
+GO
+
+IF NOT EXISTS ( SELECT 1 FROM [PowerBI].[dss-pbi-financialyear] WHERE [FinancialYear] = '2026-2027' )
+BEGIN
+
+INSERT INTO [PowerBI].[dss-pbi-financialyear]
+           ([FinancialYear]
+           ,[StartDateTime]
+           ,[EndDateTime]
+           ,[Comments],[CurrentYear])
+     VALUES
+           ('2026-2027'
+           ,'2026-04-01 00:00:00.0000000'
+           ,'2027-03-31 23:59:00.0000000'
+           ,'September 2027',1)
+END
+ELSE
+BEGIN
+    UPDATE [PowerBI].[dss-pbi-financialyear]
+    SET [CurrentYear] = 1
+    WHERE [FinancialYear] = '2026-2027';
+END
+Commit
